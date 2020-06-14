fae76a59ab1e92b7c03356d4ca2939942b821fe2
JB Bell <tquidca@gmail.com>
2020-06-13 20:46:19 -0700

    OMG THIS WORKS (-ish)

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
-import * as awsx from "@pulumi/awsx";
 
 // Below code based off the tutorial at https://www.pulumi.com/docs/tutorials/aws/ec2-webserver/
 
@@ -14,64 +13,100 @@ const ami = pulumi.output(aws.getAmi({
     mostRecent: true,
 }));
 
-// Time for network goodies
-const vpc = new awsx.ec2.Vpc("custom", {
-    subnets: [{
-        type: "public",
-        name: "subnet-www",
-    },
-    {
-        type: "public",
-        name: "subnet-bastion",
-    },
-    {
-        type: "isolated",
-        name: "subnet-app",
-    }]
+const vpc = new aws.ec2.Vpc("vpc", {
+    cidrBlock: "10.0.0.0/16",
 });
 
-const bastion_group = new aws.ec2.SecurityGroup("bastion-secgrp", {
+const web_subnet = new aws.ec2.Subnet("subnet-web", {
+    cidrBlock: "10.0.0.0/24",
+    tags: {
+        Name: "subnet-web",
+    },
+    vpcId: vpc.id,
+    availabilityZone: "ca-central-1a"
+});
+
+const bastion_subnet = new aws.ec2.Subnet("subnet-bastion", {
+    cidrBlock: "10.0.1.0/24",
+    tags: {
+        Name: "bastion-web",
+    },
+    vpcId: vpc.id,
+    availabilityZone: "ca-central-1a"
+});
+
+const app_subnet = new aws.ec2.Subnet("subnet-app", {
+    cidrBlock: "10.0.2.0/24",
+    tags: {
+        Name: "subnet-app",
+    },
+    vpcId: vpc.id,
+    availabilityZone: "ca-central-1a"
+});
+
+const gw = new aws.ec2.InternetGateway("gw", {
+    tags: {
+        Name: "gw",
+    },
+    vpcId: vpc.id,
+})
+
+const bastion_group = new aws.ec2.SecurityGroup("bastion", {
     // Repalce cidrBlocks with your own IP or CIDR range
-    ingress: [
-        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["174.6.14.57/32"] },
+    ingress: [{ 
+        protocol: "tcp",
+        fromPort: 22,
+        toPort: 22,
+        cidrBlocks: ["174.6.14.57/32"]},
     ],
     vpcId: vpc.id,
+    tags: {
+        Name: "sg_bastion",
+    },
 });
 
-const www_group = new aws.ec2.SecurityGroup("webserver-secgrp", {
-    ingress: [{
-        protocol: "tcp",
-        securityGroups: [bastion_group.name],
-        fromPort: -1,
-        toPort: 22
-    }],
+const www_group = new aws.ec2.SecurityGroup("webserver", {
     vpcId: vpc.id,
-});
-
-const app_group = new aws.ec2.SecurityGroup("appserver-secgrp", {
     ingress: [{
         protocol: "tcp",
-        securityGroups: [www_group.name],
-        fromPort: -1,
+        fromPort: 443,
+        toPort: 443,
+        cidrBlocks: ["0.0.0.0/0"],
+    },
+    {
+        protocol: "tcp",
+        fromPort: 22,
+        toPort: 22,
+        securityGroups: [bastion_group.id]
+    }],
+    tags: {
+        Name: "sg_webserver",
+    },
+});
+
+const app_group = new aws.ec2.SecurityGroup("appserver", {
+    ingress: [{
+        protocol: "tcp",
+        securityGroups: [www_group.id],
+        fromPort: 80,
         toPort: 80
     },
     {
         protocol: "tcp",
-        securityGroups: [bastion_group.name],
-        fromPort: -1,
+        securityGroups: [bastion_group.id],
+        fromPort: 22,
         toPort: 22
     }],
-    egress: [], // No backsies on the www_group!
+    egress: [{
+        protocol: "-1",
+        cidrBlocks: ["127.0.0.1/32"],
+        fromPort: 0,
+        toPort: 0,
+    }], // No backsies on the www_group!
     vpcId: vpc.id,
-});
-
-// Use this to avoid a circular reference
-const www_to_app = new aws.ec2.SecurityGroupRule("www-to-app", {
-    type: "egress",
-    protocol: "tcp",
-    fromPort: -1,
-    toPort: 80,
-    securityGroupId: www_group.name,
+    tags: {
+        Name: "sg_appserver"
+    },
 });
 
 const userData = 
@@ -80,31 +115,27 @@ sudo yum update -y
 sudo yum install -y httpd24
 sudo yum install -y curl`;
 
-const webserver = new aws.ec2.Instance("webserver-www", {
+const webserver = new aws.ec2.Instance("webserver", {
     instanceType: size,
-    securityGroups: [www_group.name], // reference the security group resource above
     ami: ami.id,
     userData: userData,
-    vpcSecurityGroupIds: [www_group.name],
-    subnetId: "subnet-www",
+    vpcSecurityGroupIds: [www_group.id],
+    subnetId: web_subnet.id,
 });
 
 const appserver = new aws.ec2.Instance("appserver", {
     instanceType: size,
-    securityGroups: [app_group.name],
     ami: ami.id,
     userData: userData,
-    vpcSecurityGroupIds: [app_group.name],
-    subnetId: "subnet-app",
+    vpcSecurityGroupIds: [app_group.id],
+    subnetId: app_subnet.id,
 });
 
 const bastion = new aws.ec2.Instance("bastion", {
     instanceType: size,
-    securityGroups: [bastion_group.name],
     ami: ami.id,
-    userData: userData,
-    vpcSecurityGroupIds: [bastion_group.name],
-    subnetId: "subnet-bastion",
+    vpcSecurityGroupIds: [bastion_group.id],
+    subnetId: bastion_subnet.id,
 });
 
 export const WebserverPublicIp = webserver.publicIp;
