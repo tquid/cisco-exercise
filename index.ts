98d623bb5953d6d8d2b7898e4516153355e94aac
JB Bell <tquidca@gmail.com>
2020-06-15 21:47:11 -0700

    Code complete.

--- index.ts
+++ index.ts
@@ -3,11 +3,16 @@ import * as pulumi from "@pulumi/pulumi";
 
 // Below code based off the tutorial at https://www.pulumi.com/docs/tutorials/aws/ec2-webserver/
 
+// User-configured variables
+let config = new pulumi.Config();
+let myCidr = config.require("myCidr");
+let myAz = config.require("myAz");
+
 const size = "t2.micro";     // t2.micro is available in the AWS free tier
 const ami = pulumi.output(aws.getAmi({
     filters: [{
         name: "name",
-        values: ["amzn-ami-hvm-*"],
+        values: ["amzn2-ami-hvm-*"],
     }],
     owners: ["137112412989"], // This owner ID is Amazon
     mostRecent: true,
@@ -23,7 +28,7 @@ const web_subnet = new aws.ec2.Subnet("subnet-web", {
         Name: "subnet-web",
     },
     vpcId: vpc.id,
-    availabilityZone: "ca-central-1a"
+    availabilityZone: myAz,
 });
 
 const bastion_subnet = new aws.ec2.Subnet("subnet-bastion", {
@@ -32,7 +37,7 @@ const bastion_subnet = new aws.ec2.Subnet("subnet-bastion", {
         Name: "subnet-bastion",
     },
     vpcId: vpc.id,
-    availabilityZone: "ca-central-1a"
+    availabilityZone: myAz,
 });
 
 const app_subnet = new aws.ec2.Subnet("subnet-app", {
@@ -41,7 +46,7 @@ const app_subnet = new aws.ec2.Subnet("subnet-app", {
         Name: "subnet-app",
     },
     vpcId: vpc.id,
-    availabilityZone: "ca-central-1a"
+    availabilityZone: myAz,
 });
 
 const igw = new aws.ec2.InternetGateway("igw", {
@@ -104,13 +109,15 @@ const app_group = new aws.ec2.SecurityGroup("appserver", {
         protocol: "tcp",
         securityGroups: [web_group.id],
         fromPort: 80,
-        toPort: 80
+        toPort: 80,
+        description: "Allow port 80 from webserver SG"
     },
     {
         protocol: "tcp",
         securityGroups: [bastion_group.id],
         fromPort: 22,
-        toPort: 22
+        toPort: 22,
+        description: "Allow port 22 from bastion SG"
     }],
     tags: {
         Name: "sg_appserver"
@@ -125,16 +132,31 @@ const world_to_web = new aws.ec2.SecurityGroupRule("world-to-web", {
     toPort: 443,
     cidrBlocks: ["0.0.0.0/0"],
     securityGroupId: web_group.id,
+    description: "Allow port 443 access to web SG globally",
 });
 
-const app_to_web_in = new aws.ec2.SecurityGroupRule("app-to-web-in", {
-    type: "ingress",
-    protocol: "tcp",
-    fromPort: 80,
-    toPort: 80,
+// We delete this after the userData load is done
+const web_to_world = new aws.ec2.SecurityGroupRule("web-to-world", {
+    type: "egress",
+    protocol: "-1",
+    fromPort: 0,
+    toPort: 0,
+    cidrBlocks: ["0.0.0.0/0"],
     securityGroupId: web_group.id,
-    sourceSecurityGroupId: app_group.id,
-});
+    description: "Allow web SG to access Internet",
+})
+
+// Instructions sepcify not to allow port 443 to web; not sure if that
+// means we *should* allow port 80. Being conservative.
+//
+// const app_to_web_in = new aws.ec2.SecurityGroupRule("app-to-web-in", {
+//     type: "ingress",
+//     protocol: "tcp",
+//     fromPort: 80,
+//     toPort: 80,
+//     securityGroupId: web_group.id,
+//     sourceSecurityGroupId: app_group.id,
+// });
 
 const bastion_to_web_in = new aws.ec2.SecurityGroupRule("bastion-to-web-in", {
     type: "ingress",
@@ -143,6 +165,7 @@ const bastion_to_web_in = new aws.ec2.SecurityGroupRule("bastion-to-web-in", {
     toPort: 22,
     securityGroupId: web_group.id,
     sourceSecurityGroupId: bastion_group.id,
+    description: "Allow ssh access from bastion SG"
 });
 
 const bastion_to_vpc = new aws.ec2.SecurityGroupRule("bastion-to-vpc", {
@@ -152,6 +175,7 @@ const bastion_to_vpc = new aws.ec2.SecurityGroupRule("bastion-to-vpc", {
     toPort: 22,
     securityGroupId: bastion_group.id,
     cidrBlocks: ["10.0.0.0/16"],
+    description: "Allow ssh out from bastion to VPC CIDR"
 });
 
 const desk_to_bastion = new aws.ec2.SecurityGroupRule("desk-to-bastion", {
@@ -159,15 +183,20 @@ const desk_to_bastion = new aws.ec2.SecurityGroupRule("desk-to-bastion", {
     protocol: "tcp",
     fromPort: 22,
     toPort: 22,
-    cidrBlocks: ["174.6.14.57/32"],
+    cidrBlocks: [myCidr],
     securityGroupId: bastion_group.id,
 });
 
 const userData = 
 `#!/bin/bash
 sudo yum update -y
-sudo yum install -y httpd24
-sudo yum install -y curl`;
+sudo yum install -y httpd
+sudo systemctl start httpd && sudo systemctl enable httpd
+sudo yum install -y mod_ssl
+cd /etc/pki/tls/certs
+sudo ./make-dummy-cert localhost.crt
+sudo sed -i.bak -e 's%SSLCertificateKeyFile /etc/pki/tls/private/localhost.key%# SSLCertificateKeyFile /etc/pki/tls/private/localhost.key%' /etc/httpd/conf.d/ssl.conf
+sudo systemctl restart httpd`;
 
 const webserver = new aws.ec2.Instance("webserver", {
     instanceType: size,
@@ -210,3 +239,4 @@ export const WebserverPublicIp = webserver.publicIp;
 export const BastionPublicIp = bastion.publicIp;
 export const WebserverPrivateIp = webserver.privateIp;
 export const AppserverPrivateIp = appserver.privateIp;
+export const webToWorldUrn = web_to_world.urn;
